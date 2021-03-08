652c1c455d9b5a52444894ad625a35c4daa6df0f
Matt McVey <matt.mcvey49@gmail.com>
2021-03-08 11:21:59 -0700

    Display filterd rooms by date on the page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
+import moment from "moment"
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import Customer from "./Customer"
@@ -12,7 +12,21 @@ const roomNumber = document.getElementById('roomNumber')
 const dateStayed = document.getElementById('dateStayed')
 const totalCost = document.getElementById('totalSpent')
 const date = document.querySelector('.date')
-const roomType = document.querySelector('.roomType')
+const startingValueDate = document.querySelector('input[type="date"]')
+const roomTypeDropDown = document.querySelector('.roomType')
+const dateInput = document.querySelector('.date-input')
+const roomsAvailable = document.querySelector('.available-rooms')
+const availableRoomNumber = document.querySelector('.room-number')
+const availableRoomType = document.querySelector('.room-type')
+const availableRoomBidet = document.querySelector('.bidet')
+const availableRoomBedSize = document.querySelector('.bed-size')
+const availableRoomNumBeds = document.querySelector('.num-beds')
+const availableRoomCost = document.querySelector('.cost')
+
+const todaysDate = new Date()
+const formattedDate = moment(todaysDate).format('YYYY/MM/DD')
+startingValueDate.value = formattedDate
+startingValueDate.min = formattedDate
 
 const allCustomersData =
   fetch("http://localhost:3001/api/v1/customers")
@@ -32,11 +46,13 @@ Promise.all([allCustomersData, allBookingsData, allRoomsData])
     const bookingData = allData[1].bookings
     const roomData = allData[2].rooms
     const totalSpent = customer.calculateAmountSpent(allData[2].rooms)
-    console.log(customer.filterRoomsByDate(bookingData, roomData, "2020/02/14"))
     displayBookedRooms(customer, bookingData)
     costOfAllRooms(customer, roomData, totalSpent)
-    roomType.addEventListener('change', filterRoomsByType)
-    date.addEventListener('change', displayMatchingRooms)
+    roomTypeDropDown.addEventListener('change', filterRoomsByType)
+    date.addEventListener('change', function() {
+      filterMatchingRooms(customer, bookingData, roomData)
+    })
+    updateCalendarDay()
   })
 
 const displayBookedRooms = (customer, bookingData) => {
@@ -51,13 +67,52 @@ const displayBookedRooms = (customer, bookingData) => {
 
 const costOfAllRooms = (customer, roomData) => {
   const total = customer.calculateAmountSpent(roomData)
-  totalCost.innerHTML +=
+  totalCost.innerHTML =
     `<article class="total-spent">$${total}</article>`
 }
 
-const displayMatchingRooms = () => {
+const updateCalendarDay = () => {
+  console.log(formattedDate)
+  // date.innerHTML = `
+  // <label for="date">Select A Date</label>
+  // <input type="date" class="date-input" name="book-date"  min="${formattedDate}">`
+}
 
-  console.log(date.value.split("-").join("/"))
+const filterMatchingRooms = (customer, bookings, rooms) => {
+  const inputDate = dateInput.value.split("-").join("/")
+  if(inputDate < formattedDate) {
+    roomsAvailable.innerText = "Please select a room in the  future!"
+  } else {
+    displayAvailableRooms(customer, bookings, rooms, inputDate)
+  }
+}
+
+const displayAvailableRooms = (customer, bookings, rooms, date) => {
+  availableRoomNumber.innerHTML = ''
+  availableRoomType.innerHTML = ''
+  availableRoomBidet.innerHTML = ''
+  availableRoomBedSize.innerHTML = ''
+  availableRoomNumBeds.innerHTML = ''
+  availableRoomCost.innerHTML = ''
+  customer.filterRoomsByDate(bookings, rooms, date).forEach(room => {
+    if(room.bidet === true) {
+      room.bidet = "Yes"
+    } else {
+      room.bidet = "No"
+    }
+    availableRoomNumber.innerHTML +=
+    `<article class="room-number">${room.number}</article>`
+    availableRoomType.innerHTML +=
+    `<article class="room-number">${room.roomType}</article>`
+    availableRoomBidet.innerHTML +=
+    `<article class="room-number">${room.bidet}</article>`
+    availableRoomBedSize.innerHTML +=
+    `<article class="room-number">${room.bedSize}</article>`
+    availableRoomNumBeds.innerHTML +=
+    `<article class="room-number">${room.numBeds}</article>`
+    availableRoomCost.innerHTML +=
+    `<article class="room-number">${room.costPerNight}</article>`
+  })
 }
 
 const filterRoomsByType = () => {
